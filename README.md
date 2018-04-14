d76a03c2fe26c14380a6feddf4ec32891e0ab12d
Francisco Albani <fran@satellogic.com>
2018-04-14 00:07:53 -0300

    First working version

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
-# archaic
+# Archaic
+
 Arch Automatic Installation Commands
+
+(Still somewhat personal)
+
+## What it does
+
+- Main Arch Linux on top of LVM on top of LUKS. Bare minimum desktop with XMonad+xmobar.
+- Secondary minimal Arch Linux as a rescue alternative.
+- refind boot manager.
+
+## Requirements
+
+- UEFI system.
+- Secure Boot disabled from UEFI.
+- GPT disk.
+- Existing EFI partition (only tested with the one created by Windows 10 in a Lenovo Thinkpad)
+- Free un-partitioned disk space ready to be asigned according instructions. See comments inside `run_archaic`.
+- Live Arch Installation ISO (only tested with a USB stick on March 2018).
+- Internet conection.
+
+**IMPORTANT:** There are many more requirements and constraints that you will only understand reading `run_archaic` and `_inside_arch_chroot`.
+
+## Usage
+
+- Put a copy of this repo inside your **running** Live Arch Installation ISO.
+- Open `run_archaic` in a text editor, read it, check it, double-check it and edit the required variables. Left `$debug=` commented, so you can do a dry-run.
+- Run `run_archaic` script. It should be in **debug** mode so you can check that all text replacements went fine.
+- Re-open `run_archaic` and uncomment the `debug=` line.
+- Run `run_archaic` script and wait for input prompts.
+- Do not relax too much.
+
+If you need to modify the code, remember: `shellcheck` is your friend.
+
+## Inspiration
+
+- https://shirotech.com/linux/how-to-automate-arch-linux-installation
+
+## Wanna help?
+
+- Smarter pacman repository to avoid redownloading packages.
+- Remove lot of duplicate and ugly bash code.
+- Ease testing with virtualization.
+- Suspend to RAM vs. crypt. What to do?
+- Many steps should be optional: backups, secondary_arch, etc.
---
+++ configs/xmonad.hs
@@ -0,0 +1,98 @@
+
+import XMonad hiding ((|||))
+import XMonad.Util.EZConfig (additionalKeys)
+import XMonad.Util.Run (spawnPipe, hPutStrLn)
+import XMonad.Actions.CycleWS (prevWS, nextWS, prevScreen, nextScreen, shiftToPrev, shiftToNext)
+import XMonad.Layout.NoBorders (smartBorders)
+import XMonad.Hooks.DynamicLog (ppTitle, ppOutput, dynamicLogWithPP, xmobarPP, xmobarColor, shorten)
+import qualified XMonad.Prompt as XP
+import XMonad.Actions.SpawnOn (shellPromptHere, spawnHere)
+import XMonad.Prompt.AppendFile (appendFilePrompt)
+import XMonad.Hooks.ManageHelpers (isFullscreen, doFullFloat)
+
+-- 2018.03.18 no logré importar sólo los que necesito:
+-- import XMonad.Hooks.ManageDocks (docks, manageDocks, avoidStruts, ToggleStruts)
+import XMonad.Hooks.ManageDocks
+
+import qualified XMonad.Layout.Decoration as XLD
+import XMonad.Layout.Grid
+import XMonad.Layout.LayoutCombinators ((|||))
+import XMonad.Layout.Named
+import XMonad.Layout.Tabbed
+import XMonad.Layout.NoBorders (smartBorders)
+
+-- Pantalla completa de Chromium:
+import XMonad.Hooks.EwmhDesktops
+
+-- 2018.03.18 necesario para evitar el problema
+-- de la fuente por default inexistente (misc-fixed 12):
+shellPromptConfig = XP.def {
+                             XP.font = "xft:xos4 Terminus:pixelsize=12:antialias=true:hinting=true"
+                           , XP.defaultText = ""
+                           }
+
+myManageHook  = manageHook defaultConfig <+>
+                manageDocks <+>
+                composeAll [isFullscreen --> doFullFloat] -- Para el fullscreen de Youtube
+
+myTheme = XLD.def { decoHeight = 16
+                  , activeColor = "#a6c292"
+                  , activeBorderColor = "#a6c292"
+                  , activeTextColor = "#000000"
+                  , inactiveBorderColor = "#000000"
+                  , XLD.fontName = "xft:xos4 Terminus:pixelsize=12:antialias=true:hinting=true"
+                  }
+
+myLayouts = avoidStruts $ (tabs ||| named "Grid" Grid)
+    where
+      tabs = smartBorders $ named "Tabs" (tabbed shrinkText myTheme)
+
+logDeco cmd = "(echo " ++ cmd ++ "; sleep 3) | /usr/bin/xmobar -o -t '}<fc=#ffff00>%StdinReader%</fc>{' -c '[Run StdinReader]'"
+spawnLog cmd = do spawn $ logDeco cmd
+                  spawnHere cmd
+
+myKeys = [
+            ((mod4Mask, xK_less),  prevWS),
+            ((mod4Mask, xK_z), nextWS),
+
+            ((mod4Mask .|. shiftMask, xK_period), shiftToNext),
+            ((mod4Mask .|. shiftMask, xK_comma),  shiftToPrev),
+
+            ((0, xK_Print ),  nextWS),
+            ((shiftMask, xK_Print),   prevWS),
+
+            ((mod4Mask, xK_Return), spawnLog "urxvtc"),
+            ((mod4Mask, xK_a), spawnLog "dbus-launch thunar"),
+            ((mod4Mask, xK_i), spawnLog "mplayer -vf screenshot -fps 30 tv://"),
+
+            -- 2015.11.02: para evitar cerrar X sin querer:
+            ((mod4Mask .|. shiftMask, xK_q), spawnLog "nada"),
+
+            -- 2015.11.15:
+            ((mod4Mask, xK_w),  nextScreen),
+            ((mod4Mask, xK_q),  prevScreen),
+
+            ((mod4Mask, xK_x), kill),
+            ((mod4Mask, xK_p), shellPromptHere shellPromptConfig),
+            ((mod4Mask, xK_b), sendMessage ToggleStruts)
+            ]
+
+main =	do
+        -- 2015.10.10: Doble topbar:
+        xmobarproc <- spawnPipe "bash -c \"tee >(xmobar -x 0 /etc/archaic_dots/xmobar/topbar) | xmobar -x 1 /etc/archaic_dots/xmobar/topbar\""
+        xmonad $ docks defaultConfig
+            {
+              terminal = "urxvtc"
+            , workspaces = map show [1..9]
+            , modMask = mod4Mask
+              -- Pantalla completa de Chromium:
+            , handleEventHook = fullscreenEventHook
+            , focusFollowsMouse = True
+            , focusedBorderColor = "#018A8"
+            , manageHook         = myManageHook
+            , logHook            = dynamicLogWithPP $ xmobarPP
+                                                        {   ppOutput = hPutStrLn xmobarproc,
+                                                            ppTitle = xmobarColor "green" "" . shorten 80
+                                                        }
+            , layoutHook         = myLayouts
+        	} `additionalKeys` myKeys
